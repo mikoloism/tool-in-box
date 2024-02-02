1a5909046ed70f5734b95f2673cb6bd56b51b295
mikoloism <mikoloism@gmail.com>
2024-02-02 22:01:27 +0330

    fix(pkg/roll): support `ComponentType` in `Roller`
    - support `HOC` type to `Out` generic type in
    	`Rollable` interface

--- packages/roll/src/roll/roller.ts
+++ packages/roll/src/roll/roller.ts
@@ -1,4 +1,12 @@
-type Fn<In extends {}, Out> = (props: In) => Out;
+type FnOut<In extends {}> =
+	| React.ReactElement
+	| JSX.Element
+	| React.ComponentType<In>
+	| Iterable<React.ReactElement>;
+
+type Fn<In extends {}, Out = FnOut<In>> = (
+	Component: React.ComponentType<In>
+) => (props: In) => Out;
 
 interface Rollable<In, Out> {
 	roll<T extends {}>(rolled: Fn<In & T, Out>): Rollable<In & T, Out>;
@@ -10,19 +18,24 @@ export class Roller<In extends {}, Out> implements Rollable<In, Out> {
 
 	constructor(rolled?: Fn<In, Out>) {
 		if (typeof rolled === "undefined") {
-			this.rolled = ((Component: In) => Component) as unknown as Fn<In, Out>;
+			this.rolled = function rolled_hoc(Component: React.FunctionComponent<In>) {
+				return Component;
+			} as Fn<In, Out>;
 		} else {
 			this.rolled = rolled;
 		}
 	}
 
 	roll<T extends {} = In>(rolled: Fn<In & T, Out>): Rollable<In & T, Out> {
-		const newRolled = (props: In & T) => this.rolled(rolled(props) as In & T);
+		const newRolled = (Component: React.ComponentType<In & T>) => {
+			const rolledHoc = rolled(Component);
+			return this.rolled(rolledHoc as any);
+		};
 
-		return new Roller<In & T, Out>(newRolled);
+		return new Roller<In & T, Out>(newRolled as Fn<In & T, Out>);
 	}
 
 	around<T>(Component: React.ComponentType<In & T>): React.ComponentType<In & T & Out> {
-		return this.rolled(Component as any) as any;
+		return this.rolled(Component as React.ComponentType<In>) as any;
 	}
 }
